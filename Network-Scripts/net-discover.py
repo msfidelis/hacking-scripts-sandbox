17415039c9742262b8100356269b0ed420e2de18
Matheus Fidelis <msfidelis@outlook.com>
2016-01-11 21:35:21 -0200

    Email food joke

---
+++ Network-Scripts/net-discover.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python2.s7
+# -*- coding: utf-8 -*-
+# Author: Matheus Fidelis aka D0ctor
+# Github: https://github.com/msfidelis
+# Web: http://nanoshots.com.br
+
+
+#Usage: python net-discover.py 192.168.1.0/24
+
+from netaddr import *
+import os, Queue, sys
+import subprocess
+
+
+def discover(subnet):
+    print '[!] Starting network Scan to %s ' %subnet
+    hosts_up = Queue.Queue()
+
+    for host in IPNetwork(subnet):
+        test = 'ping -c 1 -w2 %s >> /dev/null' % host
+        #response,result = subprocess.run([test])
+
+        response = os.system(test)
+        if response == 0:
+            print 'Host %s is UP' % host
+            hosts_up.put(host)
+        else:
+            print 'Host %s not avaliable' % host
+            pass
+    print ''
+    print '[*] LIVE HOSTS'    
+    while not hosts_up.empty():
+        up = hosts_up.get()
+        print '[+] %s ' % up
+
+
+subnet = sys.argv[1]
+discover(subnet)
---
+++ Trolling Scripts/Email Flood Trolling/Email-Flood.py
@@ -0,0 +1,83 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+from email.mime.text import MIMEText
+from email.MIMEMultipart import MIMEMultipart
+import smtplib as s
+import sys
+
+def main():
+    banner()
+    email_user = raw_input('INFORME SEU E-MAIL (GMAIL): ')
+    email_pass = raw_input('INFORME SUA SENHA: ')
+
+    validade,conn = test(email_user, email_pass)
+    print conn
+    if validade == True:
+        send_emails(email_user,conn)
+    else:
+        sys.exit
+
+#VALIDA O LOGIN E SENHA
+def test(email_user, email_pass):
+    try:
+        conn = s.SMTP('smtp.gmail.com', 587)
+        conn.starttls()
+        conn.ehlo
+        conn.login(email_user, email_pass)
+        return True,conn
+
+    except:
+        print 'FALHA NA CONEXÃO'
+        print '1º - VERIFIQUE SEU USUÁRIO E SENHA'
+        print '2º - CERTIFIQUE-SE DE QUE HABILITOU OS APLICATIVOS MENOS SEGUROS'
+        print 'URL: https://www.google.com/settings/security/lesssecureapps'
+        return False
+
+def send_emails(email_user,conn):
+    #Pega o e-mail da vítima
+    FROM = email_user
+    TO = raw_input('INFORME O DESTINATÁRIO: ')
+
+    #Escreve o e-mail
+    SUBJECT = raw_input('INFORME O ASSUNTO: ')
+    text = raw_input('ESCREVA UMA MENSAGEM: ')
+
+    #Formata a mensagem nos padrões de envio SMTP
+    message = MIMEMultipart()
+    message['From'] = FROM
+    message['To'] = TO
+    message['Subject'] = SUBJECT
+    message.attach(MIMEText(text, 'plain'))
+    email = message.as_string()
+
+    #Envia o E-mail
+    while 1:
+        try:
+            conn.sendmail(FROM, TO, email)
+            print 'Floodando seu amigo.. Pressione CTRL + C para cancelar'
+        except:
+            print 'Fail...'
+            sys.exit()
+
+
+def banner():
+    print """
+ ---|E-MAIL FLOOD SCRIPT|-----------------------------------------------------------------------------------
+|                                                                                                           |
+|   ESTE SCRIPT FOI PROJETO POR DIVERSÃO. ELE É PERFEITO PARA PEGADINHAS COM SEUS AMIGOS,                   |
+|   MANDAR (MASSIVAS) MENSAGENS DE FELIZ ANIVERSÁRIO, BOAS FESTAS, DECLARAR SEU AMOR E TROLLAR              |
+|   SEU GRUPO DE ESTUDAS DA FACULDADE.                                                                      |
+|   EU NÃO SOU RESPONSÁVEL PELO USO INCORRETO DA FERRAMENTA. ALIÁS VOCÊ PODE MODIFICAR A VONTADE            |
+|   ELA SÓ FUNCIONA COM O GMAIL E É NECESSÁRIA UMA PARAMETRIZAÇÃO DE SEGURANÇA PARA QUE ELA FUNCIONE BEM.   |
+|   SUGIRO QUE PROSSIGA COM SEU E-MAIL FAKE :)                                                              |
+|   ATIVE A OPÇÃO DE 'HABILITAR APLICATIVOS MENOS SEGUROS'                                                  |
+|   URL: https://www.google.com/settings/security/lesssecureapps                                            |
+|                                                                                                           |
+|   ATENCIOSAMENTE: D0ctor                                                                                  |
+ -----------------------------------------------------------------------------------------------------------
+
+ """
+    return True
+
+main()
