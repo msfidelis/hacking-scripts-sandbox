7bca12187d0263d544b921e88802304ec0b89f04
Matheus Fidelis <msfidelis@outlook.com>
2016-01-27 21:47:58 -0200

    rsync

--- Backup/backup-diferencial.py
+++ Backup/backup-diferencial.py
@@ -6,6 +6,48 @@ Script de Automação do Backup Full semanal e criação de Logs do Fileserver
 para melhorar o script antigo escrito em Shellscript utilizando compactação via tar.gz
 
 Talvez o ultimo script que eu escrevo como estagiário :)
+
+
+  ===========================================================================
+|| CONSIDERAÇÕES IMPORTANTES SOBRE O RSYNC
+||  O Script pode ser modificado a vontade. Porém, ele foi criado de antemão
+||  para os seguintes parâmetros que podem ser modificados na variável 'opts'
+||  Dentro da função gerabackup(). Aqui vai uma explicação bem rápida sobre
+||  cada um deles:
+||
+||  -v: Ativa a “verbosidade”. Simplesmente faz o rsync relatar todo o
+||  processo na tela.
+||
+||  -q: Faz rsync trabalhar quietinho. Ele só printa alguns logs no final do
+||  script. Ativem a opção e removam o -v caso queiram evitar logs muito grandes
+||  no servidor :)
+||
+||   -r: Esse parâmetro é muito importante. Pois ele garante a recursividade do
+||   Rsync. Ou seja, faz ele procurar arquivos e pastas dentro dos subdiretórios
+||   Caso essa opção não seja ativada o Rsync vai sincronizar somente o primeiro
+||   nível dos diretórios, ignorando as subpastas.
+||
+||  -n: rodada de testes. Serve para você testar o rsync sem correr o risco dele
+||   modificar seus arquivos.
+||
+||   –delete: apaga arquivos do destino que não existam na origem. Este comando
+||    garante que a origem e destino sejam exatamente iguais depois da sincronização.
+||    Sem isso, o destino acumularia com arquivos que não existem mais na origem.
+||    Desativá-lo pode ser útil para backups diários para manter um histórico.
+||
+||    -z: ativa a compressão, torna a transferência mais rápida.
+||
+||    -l, --links - cópia symlinks como symlinks
+||
+||    -L, --copy-links - transforma symlink em sua referência, arquivo ou diretório
+||
+||    -t, --times - preserva a data de modificação;
+||
+||    --exclude - exclui do backup alguns tipos de arquivos identificados pelo nome
+||    pode ser utilizado para ignorar arquivos temporários e logs, como por exemplo
+      --exclude={'*.tmp,*.log'}
+||
+
 '''
 
 import subprocess
@@ -64,16 +106,17 @@ def geralog():
 #CONSTROI O ARQUIVO E PATH DE BACKUP E RETORNA
 def gerabackup():
     date = (time.strftime("%Y-%m-%d"))
-    backupfile  = '%s-backup-full.tar.gz' % date    # Cria o nome do arquivo de Backup
-    pathdestino = '/mnt/hdbackup/%s' % backupfile   # Destino onde será gravado o Backup
+    opts = '-vzr'                                   # Opções que serão passadas com Rsync. Comentários no inicio do Script :)
+    exclude = '*.log, *.tmp, .recycle'              # Define os diretórios e tipos de arquivos que não vão ter backup
+    pathdestino = '/mnt/backupclone/%s'             # Destino onde será gravado espelhado o backup
     pathorigem  = '/mnt/storage/'                   # pasta que será 'backupeada'
-    backup      = 'tar cvf %s %s' % (pathdestino, pathorigem) # Comando de execução
+    backup      = 'rsync -%s --exclude={%s} %s %s' % (opts, exclude, pathdestino, pathorigem) # Comando de execução
 
     return backup
 
 
 #CRIA OS BACKUPs
-def backupfull():
+def backupclone():
     horaInicio  = time.strftime('%H:%M:%S')
     pathlog     = geralog()
     backup      = gerabackup()
@@ -97,4 +140,4 @@ def backupfull():
 
 
 
-backupfull()
+backupclone()
